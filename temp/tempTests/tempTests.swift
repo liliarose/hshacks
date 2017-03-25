a05ae17b3075c7292cf108f5a91a740d60ac2493
sophia17yuan <sophia.15yuan@gmail.com>
2017-03-25 11:48:24 -0700

    temp is only a temporary name

---
+++ temp/tempTests/tempTests.swift
@@ -0,0 +1,36 @@
+//
+//  tempTests.swift
+//  tempTests
+//
+//  Created by Marshmallow san .。.:*・°☆ on 3/25/17.
+//  Copyright © 2017 Marshmallow san .。.:*・°☆. All rights reserved.
+//
+
+import XCTest
+@testable import temp
+
+class tempTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
