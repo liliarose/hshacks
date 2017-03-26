8003bfb90b16059147c2a2a68210bab52f4cd49e
sophia17yuan <sophia.15yuan@gmail.com>
2017-03-25 23:14:34 -0700

    added bootstrap login & reg. templates

---
+++ temp/labelCode.swift
@@ -0,0 +1,9 @@
+//
+//  labelCode.swift
+//  temp
+//
+//  Created by Marshmallow san .。.:*・°☆ on 3/25/17.
+//  Copyright © 2017 Marshmallow san .。.:*・°☆. All rights reserved.
+//
+
+import Foundation
